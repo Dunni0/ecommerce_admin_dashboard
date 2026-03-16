e2671ae13e9bfeaa64954829ca7284c92107eb5e
Dunni's Mac <pelumioladunni3@gmail.com>
2026-03-16 13:23:48 +0100

    update - responsiveness

---
+++ dashboard.js
@@ -0,0 +1,19 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu_btn");
+const closeBtn = document.querySelector("#close_btn");
+const themeToggler = document.querySelector(".theme_toggler");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
